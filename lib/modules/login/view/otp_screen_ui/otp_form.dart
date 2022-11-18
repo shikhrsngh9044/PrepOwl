8660506378e3bb215df90d2ef97f3bd0928abeed
Anamika Verma <anamikaverma@Anamikas-MacBook-Pro.local>
2022-11-18 10:29:18 +0530

    chnages

--- lib/modules/login/view/otp_screen_ui/otp_form.dart
+++ lib/modules/login/view/otp_screen_ui/otp_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:prepowl/_utils/helpers/global.dart';
 import '../../controller/login_bloc.dart';
 import '../../../welcome/welcome.dart';
 import '../../../../_utils/configs/theme_config.dart';
@@ -217,7 +218,7 @@ class _OtpFormState extends State<OtpForm> {
         return GestureDetector(
           onTap: () {
             if (widget.state.showResendOtp == true) {
-              print("Resend OTP Clicked");
+              printDebug("Resend OTP Clicked");
             }
           },
           child: Text(
--- lib/modules/login/view/social_screen_ui/socal_sign_up.dart
+++ lib/modules/login/view/social_screen_ui/socal_sign_up.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-import '../../../../_utils/configs/theme_config.dart';
+import 'package:prepowl/modules/login/view/social_screen_ui/social_icon.dart';
 import '../../../../_utils/constants/asset_constants.dart';
 import 'or_divider.dart';
 
@@ -18,43 +16,13 @@ class SocalLogin extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 2,
-                    color: AppTheme.primaryColorLight,
-                  ),
-                  shape: BoxShape.circle,
-                ),
-                child: SvgPicture.asset(
-                  Assets.facebookLogin,
-                  height: 20,
-                  width: 20,
-                ),
-              ),
+            SocalIcon(
+              iconSrc: Assets.facebookLogin,
+              press: () {},
             ),
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 2,
-                    color: AppTheme.primaryColorLight,
-                  ),
-                  shape: BoxShape.circle,
-                ),
-                child: SvgPicture.asset(
-                  Assets.gmailLogin,
-                  height: 20,
-                  width: 20,
-                ),
-              ),
+            SocalIcon(
+              iconSrc: Assets.gmailLogin,
+              press: () {},
             ),
           ],
         ),
